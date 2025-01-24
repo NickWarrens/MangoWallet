dc21010217090012bef909ac33ecab0344118433
Nick Warrens <nick.warrens@student.kdg.be>
2025-01-25 00:23:17 +0100

    Added static files in wwwroot

---
+++ UI.MVC/wwwroot/js/popus.js
@@ -0,0 +1,34 @@
+﻿const exchangeModal = document.getElementById("exchange-modal");
+const transferModal = document.getElementById("transfer-modal");
+
+const exchangeBtn = document.getElementById("exchange-btn");
+const transferBtn = document.getElementById("transfer-btn");
+
+const closeExchange = document.getElementById("close-exchange");
+const closeTransfer = document.getElementById("close-transfer");
+
+exchangeBtn.addEventListener("click", () => {
+    exchangeModal.style.display = "flex";
+});
+
+transferBtn.addEventListener("click", () => {
+    transferModal.style.display = "flex";
+});
+
+
+closeExchange.addEventListener("click", () => {
+    exchangeModal.style.display = "none";
+});
+
+closeTransfer.addEventListener("click", () => {
+    transferModal.style.display = "none";
+});
+
+window.addEventListener("click", (event) => {
+    if (event.target === exchangeModal) {
+        exchangeModal.style.display = "none";
+    }
+    if (event.target === transferModal) {
+        transferModal.style.display = "none";
+    }
+});
